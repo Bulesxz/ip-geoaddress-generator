210f18d3390c55e6fec0fd663417903c5e7dc0e9
Bulesxz <1252455742@qq.com>
2025-06-10 00:17:44 +0800

    fix: 修改关键词

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "真实地址生成器",
   description: "基于IP地址生成真实地址",
   keywords:
-    "IP地址, 地址生成器, 地理位置, IP定位, IP查询, IP地址查询, 地理位置查询, 位置生成器, 地址查询工具, IP工具, 地址定位, 位置信息",
+    "在线地址生成, IP地址, 地址生成器, 地理位置, IP定位, IP查询, IP地址查询, 地理位置查询, 位置生成器, 地址查询工具, IP工具, 地址定位, 位置信息",
   authors: [{ name: "GuooGaii" }],
   openGraph: {
     title: "真实地址生成器",
